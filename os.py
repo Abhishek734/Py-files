eba98a3b939bb4d82dc4de5e35157f42202ce226
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 18:13:23 +0530

    Update os.py

--- os.py
+++ os.py
@@ -1,7 +1,7 @@
 import os
 
 # print(dir(os))
-print(os.getcwd()) # current dir
+# print(os.getcwd()) # current dir
 # os.chdir('C:/Users/Acer/Desktop/') # change dir
 # print(os.getcwd())
 # print(os.listdir()) # return list of dir
@@ -25,5 +25,21 @@ print(os.getcwd()) # current dir
 
 # print(os.listdir())
 os.chdir('C:/Users/Acer/Desktop/') # change dir
-print(os.getcwd())
-os.walk()
+# print(os.getcwd())
+# for dirpath, dirnames, filenames in os.walk('C:/Users/Acer/Desktop/'):                # os.walk(path) //usefull
+#     print("Current path: ", dirpath)
+#     print("Directories: ", dirnames)
+#     print("Files: ", filenames)
+
+# print(os.environ.get("PUBLIC"))
+# file_path = os.path.join(os.environ.get("PUBLIC"), 'test.txt') # os.path.join
+# print(file_path)
+
+# with open(file_pathm,"w") as f:
+#     f.write()
+
+print(os.path.split("/tmp/test.txt")) # os.path.split
+print(os.path.exists("/tmp/test.")) # os.path.exists #os.path.isdir #os.path.isfile
+
+print(os.path.splitext("/tmp/test.txt")) # returns ('/tmp/test', '.txt')
+print(dir(os.path)) # read all the dirs in os.path
