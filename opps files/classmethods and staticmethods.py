62f9a6f01fe0524ffc6bb4ce42d1a5c994a4f1e7
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 19:38:37 +0530

    Create classmethods and staticmethods.py

---
+++ opps files/classmethods and staticmethods.py
@@ -0,0 +1,42 @@
+#classmethods and staticmethods.py
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self,first,last,pay):                 # constructor self == instance
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@gmail.com'
+
+        Employee.num_of_emps += 1                       # not self as we don't have to change it for every instances
+
+    def fullname(self): # dont forget self
+        return '{} {}'.format(self.first,self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount): # cls class Variable //self for instance varibale
+        cls.raise_amount = amount
+
+# instances
+
+emp_1 = Employee("Avinas","kumar",50000)  # self passed automatically
+emp_2 = Employee("deepa","kumari",40000)
+
+# Employee.set_raise_amount(1.05) # set_raise_amount --> class method {== Employee.raise_amount = 1.05}
+
+# # emp_1.set_raise_amount(1.05) # same results
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+
