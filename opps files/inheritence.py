b130cdfbb4f13e3ab328e73b937a2d76430d3f95
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 20:41:07 +0530

    Update inheritence.py

--- opps files/inheritence.py
+++ opps files/inheritence.py
@@ -22,14 +22,50 @@ class Developers(Employee):                                 # simple inherit Emp
         # Employee.__init__(Self, first,last,pay)           # does the same
         self.prog_lang = prog_lang
 
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees = None):
+        super().__init__(first,last,pay)                    # wow super()
+        # Employee.__init__(Self, first,last,pay)           # does the same
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emp(self):
+        for emp in self.employees:
+            print("--->",emp.fullname())
 
 dev_1 = Developers('Corey', 'Schafer', 50000,"python")
 dev_2 = Developers('Test', 'Employee', 60000,"java")
 
+mgr_1 = Manager("sue", "smith", 900000,[dev_1])
+
+# is instance
+print(isinstance(mgr_1, Manager)) # True
+print(isinstance(mgr_1, Employee)) # True
+print(isinstance(mgr_1, Developers)) # false
+# issubclass --> to check subclass
+print(issubclass(Developers, Employee))  #True
+print(issubclass(Manager, Employee)) # True
+print(issubclass(Manager,Developers)) # False
+# print(mgr_1.email)
+# mgr_1.add_emp(dev_2)
+# mgr_1.remove_emp(dev_1)
+# mgr_1.print_emp()
+
+
 # help(Developers)
 
-print(dev_1.email)
-print(dev_1.prog_lang)
+# print(dev_1.email)
+# print(dev_1.prog_lang)
 
 # print(dev_1.pay)
 # dev_1.apply_raise()
