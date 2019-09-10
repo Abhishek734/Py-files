d528934362255dafc82545005253c4fc06bec841
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 17:19:13 +0530

    Update os.py

--- os.py
+++ os.py
@@ -4,7 +4,20 @@ import os
 print(os.getcwd()) # current dir
 os.chdir('C:/Users/Acer/Desktop/') # change dir
 print(os.getcwd())
-print(os.listdir()) # return list of dir
+# print(os.listdir()) # return list of dir
 
 # os.mkdir("NST-2/sub-Dir-2") # making new dir
-os.makedirs("NST-2/sub-Dir-2") # making new dir
+# os.makedirs("NST-2/sub-Dir-2") # making new dir //prefered
+
+# os.rmdir("NST-2/sub-Dir-2") # delete dirs // prefered
+# os.removedirs("NST-2/sub-Dir-2") # delete dirs
+
+# print(os.listdir())
+
+# os.rename("BasicResume.pdf","Basic_resume.pdf")
+
+print(os.stat("Basic_resume.pdf").st_size)
+print(os.stat("Basic_resume.pdf").st_mtime)
+
+from datetime import datetime
+print(datetime.fromtimestamp(os.stat("Basic_resume.pdf").st_mtime))
