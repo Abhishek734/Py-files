763043dc85affe813af71e04227e630ee21442ef
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 18:31:21 +0530

    Update classes and instances.py

--- opps files/classes and instances.py
+++ opps files/classes and instances.py
@@ -1,24 +1,28 @@
 # class (blueprint)
 
 class Employee:
-    pass
+    def __init__(self,first,last,pay):                 # constructor self == instance
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@gmail.com'
 
 # instances
-emp_1 = Employee()
-emp_2 = Employee()
+emp_1 = Employee("Avinas","kumar",50000)  # self passed automatically
+emp_2 = Employee("deepa","kumari",40000)
 
 print(emp_1)
 print(emp_2)
 
-emp_1.first = "Abhi"
-emp_1.last = "prasad"
-emp_1.email = "Abhiprasad@gmail.com"
-emp_1.pay = 700000
+# emp_1.first = "Abhi"
+# emp_1.last = "prasad"
+# emp_1.email = "Abhi.prasad@gmail.com"
+# emp_1.pay = 700000
 
-emp_2.first = "Anu"
-emp_2.last = "kumari"
-emp_2.email = "Anukumar2@gmail.com"
-emp_2.pay = 400000
+# emp_2.first = "Anu"
+# emp_2.last = "kumari"
+# emp_2.email = "Anu.kumar2@gmail.com"
+# emp_2.pay = 400000
 
 print(emp_1.email)
 print(emp_2.email)
