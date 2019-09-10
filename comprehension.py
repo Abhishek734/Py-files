d2b46ee60c334127b2a951c32ab1360e4841f75d
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 15:54:30 +0530

    Create comprehension.py

---
+++ comprehension.py
@@ -0,0 +1,20 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+## option 1
+
+# my_list = []
+# for n in nums:
+#     my_list.append(n)
+
+# print(my_list)
+# list comprehension
+
+# my_list = [n for n in nums]
+# print(my_list)
+
+# my_squares = [n*n for n in nums]
+# print(my_squares)
+
+# #map and lambda(func)
+# my_list = map(lambda n: n*n, nums)
+# print(list(my_list))
