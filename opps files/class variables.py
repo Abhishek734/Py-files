3a834596bb34939d4eb4e03056fb607a9673f218
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 18:59:56 +0530

    Update class variables.py

--- opps files/class variables.py
+++ opps files/class variables.py
@@ -3,6 +3,7 @@
 
 class Employee:
 
+    num_of_emps = 0
     raise_amount = 1.04
 
     def __init__(self,first,last,pay):                 # constructor self == instance
@@ -11,13 +12,16 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@gmail.com'
 
+        Employee.num_of_emps += 1                       # not self as we don't have to change it for every instances
+
     def fullname(self): # dont forget self
         return '{} {}'.format(self.first,self.last)
 
     def apply_raise(self):
         # self.pay = int(self.pay * Employee.raise_amount)
-        self.pay = int(self.pay * self.raise_amount)
+        self.pay = int(self.pay * self.raise_amount) # self.raise_amount gives ability to change for a single instanse
 # instances
+print(Employee.num_of_emps)
 emp_1 = Employee("Avinas","kumar",50000)  # self passed automatically
 emp_2 = Employee("deepa","kumari",40000)
 
@@ -34,3 +38,5 @@ print(emp_2.raise_amount)
 print(emp_1.__dict__)
 print(emp_2.__dict__)
 # print(Employee.__dict__)
+
+print(Employee.num_of_emps)
