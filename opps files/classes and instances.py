b3516fc98c944cdc932e8404afb85c59da9f3fd1
Abhishek734 <abhishekprasad734@gmail.com>
2019-09-10 18:38:49 +0530

    Update classes and instances.py
    classes, instances , methods

--- opps files/classes and instances.py
+++ opps files/classes and instances.py
@@ -7,12 +7,15 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@gmail.com'
 
+    def fullname(self): # dont forget self
+        return '{} {}'.format(self.first,self.last)
+
 # instances
 emp_1 = Employee("Avinas","kumar",50000)  # self passed automatically
 emp_2 = Employee("deepa","kumari",40000)
 
-print(emp_1)
-print(emp_2)
+# print(emp_1)
+# print(emp_2)
 
 # emp_1.first = "Abhi"
 # emp_1.last = "prasad"
@@ -24,5 +27,9 @@ print(emp_2)
 # emp_2.email = "Anu.kumar2@gmail.com"
 # emp_2.pay = 400000
 
-print(emp_1.email)
-print(emp_2.email)
+# print(emp_1.email)
+# print(emp_2.email)
+print(emp_1.fullname())
+# print(emp_2.fullname())
+
+print(Employee.fullname(emp_1))
